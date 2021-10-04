0a91e98d866e8b7e9f25220ea428c2f2e05346e2
Payton Macdonald <paytonmac@msn.com>
2021-10-04 13:36:00 -0600

    David Fix 2

--- CKK.Logic/Models/ShoppingCart.cs
+++ CKK.Logic/Models/ShoppingCart.cs
@@ -65,13 +65,17 @@ namespace CKK.Logic.Models
             var item1 = GetProductById(id);
             if (item1 == null)
                 throw new ProductDoesNotExistException();
-            item1.Quantity -= quant;
-            if (item1.Quantity <= 0)
+            if (item1.Quantity - quant <= 0)
             {
                 item1.Quantity = 0;
                 Products.Remove(item1);
             }
+            else
+            {
+                item1.Quantity = item1.Quantity - quant;
+            }
             return item1;
+
         }
         public decimal GetTotal()
         {
